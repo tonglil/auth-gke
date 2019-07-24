ef524a45fd53d85ad323b4eea68e1654345f3014
Tony Li <tony.li@nytimes.com>
2019-07-23 18:48:32 -0700

    fix ZONE parameter

--- auth-gke.sh
+++ auth-gke.sh
@@ -2,9 +2,8 @@
 set -euo pipefail
 
 main() {
-  _trace gcloud container clusters get-credentials "$CLUSTER" --project "$PROJECT" --zone "${LOCATION:-$ZONE}"
+  _trace gcloud container clusters get-credentials "$CLUSTER" --project "$PROJECT" --zone "${LOCATION:-${ZONE:-}}"
 }
-
 
 # Display the underlying command
 _trace() {

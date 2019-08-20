01bde191744b7c3b9280bdb5ab61371ac57e1adf
Tony Li <tony.li@nytimes.com>
2019-08-19 19:52:55 -0700

    print kubectl version

--- auth-gke.sh
+++ auth-gke.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 main() {
   _trace gcloud container clusters get-credentials "$CLUSTER" --project "$PROJECT" --zone "${LOCATION:-${ZONE:-}}"
+  # Display after getting credentials because gcloud sets the version based on the cluster version
+  _trace kubectl version
 }
 
 # Display the underlying command
